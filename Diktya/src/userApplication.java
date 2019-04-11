4ea29a16ded92766bbcbfb75846ef13e2e0b151d
mpountou <giannis.boudou@gmail.com>
2019-04-11 22:01:55 +0300

    echo response

--- Diktya/src/userApplication.java
+++ Diktya/src/userApplication.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+import javax.sound.sampled.*;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 
 public class userApplication {
+    public static void main(String[] param) {
 
-    public static void main(String[] args) {
-        System.out.println("first commit");
+        int serverPort= 38025;
+        int clientPort= 48025;
+        // echo code with delay
+        String echo_code_delay = "E9307\n";
+        String echo_code_without_delay = "E0000\n";
+
     }
+
+
+
 }
