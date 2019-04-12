2e7945cdb4052bb6fde600fef946eefa47130b3c
mpountou <giannis.boudou@gmail.com>
2019-04-12 03:34:09 +0300

    echo response + matlab

--- matlab/g1.m
+++ matlab/g1.m
@@ -3,10 +3,10 @@ fileID = fopen('g1s1.txt','r');
 formatSpec = '%f';
 A = fscanf(fileID,formatSpec);
 plot(A,'Color','black');
-set(gca,'XTick',[0:20:200])
+set(gca,'XTick',[0:20:220])
 set(gca,'YTick',[500:100:2300]) 
 ylim([500 2300])
-xlim([0 200])
+xlim([0 220])
 fclose(fileID);
 %% Session 2
 
