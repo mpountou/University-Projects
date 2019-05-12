a17017f0ea5e28968628f479c8c70798a2fa456a
mpountou <giannis.boudou@gmail.com>
2019-05-12 23:55:06 +0300

    diagram g14

---
+++ matlab/diagrams/g14/g14.m
@@ -0,0 +1,16 @@
+% AQDCPM subs CODE SONG 9271
+% g14s1.txt == AQDCPM_SUBS_CODE_XXXX_SONG.txt
+fileID = fopen('g14s1.txt');
+mydata = textscan(fileID,'%f%f','HeaderLines',1);
+fclose(fileID);
+columns = cell2mat(mydata);
+column_1 = columns(:,1);
+column_2 = columns(:,2);
+j = 1 ;
+for i = 1: size(column_1,1)
+    a(j) = column_1(i) ;
+    j = j + 1 ;
+    a(j) = column_2(i) ;
+    j = j+1;
+end
+histogram(a,'FaceColor','black','FaceAlpha',0.95);
--- matlab/g14.m
+++
@@ -1,9 +0,0 @@
-% AQDCPM subs CODE song 9271
-j = 1 ;
-for i = 1: size(VarName24,1)
-    a(j) = VarName24(i) ;
-    j = j + 1 ;
-    a(j) = VarName25(i) ;
-    j = j+1;
-end
-histogram(a,'FaceColor','black','FaceAlpha',0.95);
