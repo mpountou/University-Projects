09fa99d5f8d18703efb27198d909cd247be07a92
mpountou <giannis.boudou@gmail.com>
2019-05-12 23:37:39 +0300

    diagram g10

--- matlab/diagrams/g10and11/g10and11.m
+++ matlab/diagrams/g10and11/g10and11.m
@@ -1,6 +1,6 @@
-% DCPM SAMPLE CODE FREQ 9271
-% g9s1.txt == DPCM_SAMPLE_CODE_XXXX_FREQ.txt
-fileID = fopen('g9s1.txt');
+% DCPM SAMPLE CODE SONG XXXX
+% g10s1.txt ==  DPCM_SAMPLE_CODE_XXXX_SONG.txt
+fileID = fopen('g10s1.txt');
 mydata = textscan(fileID,'%f%f','HeaderLines',1);
 fclose(fileID);
 columns = cell2mat(mydata);
@@ -13,5 +13,9 @@ for i = 1: size(column_1,1)
     a(j) = column_2(i) ;
     j = j+1;
 end
+%step 1 for G10
 plot(a,'Color','black');
-xlim([120000 123000]);
+xlim([0 250000]);
+figure;
+%step 2 for G11
+histogram(a,'FaceColor','black','FaceAlpha',1);
