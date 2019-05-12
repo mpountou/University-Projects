7fdc1c21609918413920c1d2c834ec8b6098d717
mpountou <giannis.boudou@gmail.com>
2019-05-12 23:52:47 +0300

    diagram g13

--- matlab/diagrams/g13/g13.m
+++ matlab/diagrams/g13/g13.m
@@ -1,6 +1,6 @@
-% DCPM SAMPLE CODE FREQ 9271
-% g9s1.txt == DPCM_SAMPLE_CODE_XXXX_SONG
-fileID = fopen('g9s1.txt');
+% AQDCPM SAMPLE CODE SONG 9271
+% g13s1.txt == AQDCPM_SAMPLE_CODE_XXXX_SONG.txt
+fileID = fopen('g13s1.txt');
 mydata = textscan(fileID,'%f%f','HeaderLines',1);
 fclose(fileID);
 columns = cell2mat(mydata);
@@ -13,5 +13,4 @@ for i = 1: size(column_1,1)
     a(j) = column_2(i) ;
     j = j+1;
 end
-plot(a,'Color','black');
-xlim([120000 123000]);
+histogram(a,'FaceColor','black','FaceAlpha',0.95);
